d3409c17169ab2782160a7b7650acbc45b4c83ed
Biswajit Mondal <bisw271923@gmail.com>
2018-08-12 23:11:30 -0700

    Delete client.php

--- gearman-aws-sqs-integration/client.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('GEARMAN_ROOT', __DIR__);
-
-require GEARMAN_ROOT . '/config.php';
-require_once GEARMAN_ROOT . '/fa_autoload.php';
-
-$client = new FaGearmanClient();
-$client->processClient();
